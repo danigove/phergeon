cf3b2ea7d25dda05a0e7c0dc9f66b3e41df9ede9
Daniel Gómez Vela <dani5002@hotmail.com>
2018-03-29 12:54:09 +0200

    Botón de registrar en el login.

--- views/site/login.php
+++ views/site/login.php
@@ -35,10 +35,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+
+                <?= Html::a('¡Registrate!', ['usuarios/create'], ['class' => 'btn btn-success']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
+
 
     <div class="col-lg-offset-1" style="color:#999;">
         You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
