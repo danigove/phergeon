f124014b06ab126260ec56842f1170fe9d49dd19
Daniel Gómez Vela <dani5002@hotmail.com>
2018-04-29 11:39:42 +0200

    Guarda coordenadas del usuario al crearse el usuario.

--- views/usuarios/_form.php
+++ views/usuarios/_form.php
@@ -6,8 +6,46 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuarios */
 /* @var $form yii\widgets\ActiveForm */
-?>
+// $url = Url::to(['razas/razasion']);
+$js = <<<EOT
 
+
+    function getLocation(){
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(obtenerPosicion, errorPosicion);
+        }else{
+            alert("quillo esto hace falta");
+        }
+    }
+
+    function errorPosicion(){
+        alert('Es necesario que permitas el acceso al localizador para que la aplicacion funcione correctamente, por favor, recarga la pagina y acepta los permisos.')
+    }
+
+    function obtenerPosicion(position) {
+        if(position != null){
+            $('#usuarios-posy').attr('value', position.coords.latitude);
+            $('#usuarios-posx').attr('value', position.coords.longitude);
+            $('.btn-success').submit();
+
+        }else{
+            console.log(position);
+        }
+    }
+
+    $('.btn-success').on('click', function(e){
+        e.preventDefault();
+        getLocation();
+    })
+
+EOT;
+
+$this->registerJs($js);
+
+?>
+<div id="demo">
+    a
+</div>
 <div class="usuarios-form">
 
     <?php $form = ActiveForm::begin(); ?>
@@ -21,6 +59,10 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'posx')->hiddenInput(['maxlength' => true])->label(false) ?>
+
+    <?= $form->field($model, 'posy')->textInput(['maxlength' => true])->label(false) ?>
 
     <?= $form->field($model, 'foto')->fileInput() ?>
 
