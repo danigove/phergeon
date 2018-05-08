c86b0464fa3d28fafec8bbefadc4eb028c06e1a4
Daniel Gómez Vela <dani5002@hotmail.com>
2018-05-08 15:41:37 +0200

    Ligeros cambios.

--- views/animales/_animal.php
+++ views/animales/_animal.php
@@ -1,4 +1,13 @@
 <?php
+use yii\helpers\Html;
+
+$css = <<<EOT
+div img {
+    width : 25%;
+}
+EOT;
+
+$this->registerCss($css);
 
 ?>
 <div class='panel panel-primary'>
@@ -7,9 +16,12 @@
     </div>
 
     <div class='panel-body'>
+        <img src="<?=$model->rutaImagen?>"/>
         <span><?= $model->distancia() ?></span>
     </div>
     <div>
         <span>Subido por <?=$model->usuario->nombre_usuario ?></span>
+        <?= Html::a('Ver', ['animales/view', 'id' => $model->id]) ?>
+
     </div>
 </div>
