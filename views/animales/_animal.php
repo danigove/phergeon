3fe7b23c7da68ff8cfed54fbd9d1e07250e3e9b8
Daniel Gómez Vela <dani5002@hotmail.com>
2018-06-07 20:15:42 +0200

    Cambio

--- views/animales/_animal.php
+++ views/animales/_animal.php
@@ -17,38 +17,30 @@ $this->registerCss($css);
     </div>
     <div class="borde">
 
-    <div class='panel-body'>
-        <div class="row">
-            <div class="col-md-4">
-                <img class="imAnimal" src="<?=$model->rutaImagen?>"/>
-            </div>
-            <div class="col-md-8">
-                <p>
-                    <?= $model->raza0->denominacion_raza ?>
-                </p>
-                <p>
-                    <?= $model->edad > 0 ? $model->edad . ' años.' : 'Menos de un año.'  ?>
-                </p>
-                <p>
-                    <?= Html::encode(substr($model->descripcion,0,27) . '...' ) ?>
-                </p>
+        <div class='panel-body'>
+            <div class="row">
+                <div class="col-md-4 col-xs-12">
+                    <img class="imAnimal" src="<?=$model->rutaImagen?>"/>
+                </div>
+                <div class="col-md-8 col-xs-12 cuerAni">
+                    <p>
+                        <?= $model->raza0->denominacion_raza ?>
+                    </p>
+                    <p>
+                        <?= $model->edad > 0 ? $model->edad . ' años.' : 'Menos de un año.'  ?>
+                    </p>
+                    <p>
+                        <?= Html::encode(substr($model->descripcion,0,27) . '...' ) ?>
+                    </p>
+                </div>
             </div>
         </div>
+        <div class='fin-panel'>
+            <span class="donante">Subido por <?=$model->usuario->nombre_usuario ?></span>
+            <span >
+                <?= Html::a('Ver', ['animales/view', 'id' => $model->id], ['class'=>'btn btn-primary seeAnimal']) ?>
+            </span>
 
-
-        <!-- <?php if(!Yii::$app->user->isGuest): ?>
-        <span><?= $model->distancia() ?></span>
-    <?php else: ?>
-        <span><?= $model->usuario->rol ?></span>
-        <?php endif ?> -->
+        </div>
     </div>
-    <div class='fin-panel'>
-        <span class="donante">Subido por <?=$model->usuario->nombre_usuario ?></span>
-        <span >
-            <?= Html::a('Ver', ['animales/view', 'id' => $model->id], ['class'=>'btn btn-primary seeAnimal']) ?>
-        </span>
-
-    </div>
-</div>
-
 </div>
