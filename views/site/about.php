3f549acef8efb850f49241708c3b4c5209ddaa6b
Daniel Gómez Vela <dani5002@hotmail.com>
2018-06-09 23:11:29 +0200

    Uso leve de metadatos

--- views/site/about.php
+++ views/site/about.php
@@ -11,31 +11,34 @@ $this->registerCssFile("@web/css/about.css", [
 $this->title = 'Sobre nosotros';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about ">
+<div class="site-about" itemscope itemtype="http://schema.org/Organization">
     <div class="row">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div id="prueba" class="col-md-8 col-xs-6">
-            <h3>¿Qué es Phergeon?</h3>
+            <h3><span itemprop="name">¿Qué es Phergeon?</span></h3>
                 <p>
                     Phergeon es el resultado del proyecto desarrollado por
-                    <a href="">Daniel Gómez Vela</a> con el objetivo de poderle encontrar
+                    <a href=""><span itemprop="author">Daniel Gómez Vela</span></a> con el objetivo de poderle encontrar
                     una familia a aquellos animales que encontremos abandonados o que no nos podamos hacer más cargo de ellos.
                 </p>
 
             <div class="col-xs-12">
 
             <h3>¿Cual es el propósito de Phergeon?</h3>
-                <p>Esta página ha sido creada con el propósito de centralizar todos esos
+                <p><span itemprop="description">Esta página ha sido creada con el propósito de centralizar todos esos
                     gritos de ayuda que, por desgracia, acostumbramos a ver en distintas
                     redes sociales (Facebook, Twitter, Instagram, etc).
+                    </span>
                 </p>
 
                 <p>
-                    La página tambien tiene como objetivo que aquellas personas que tengan el deseo de
-                    añadir un miembro más a la familia puedan tener un sitio en el que esté concentrado todos aquellos animales
-                    que estén buscando a alguien que los quiera, en vez de ir buscando por distintas redes sociales, o dándote una vuelta por el pueblo/ciudad.
+                    <span itemprop="description">
+                        La página tambien tiene como objetivo que aquellas personas que tengan el deseo de
+                        añadir un miembro más a la familia puedan tener un sitio en el que esté concentrado todos aquellos animales
+                        que estén buscando a alguien que los quiera, en vez de ir buscando por distintas redes sociales, o dándote una vuelta por el pueblo/ciudad.
+                    </span>
                 </p>
             </div>
             </div>
